e9f9eb0db94754db0e2f4d1318cb82abde5165e2
hyunseong <oohs942230@naver.com>
2025-07-16 08:51:43 +0900

    수정 및 커밋 메시지 연습

--- chap02/src/sec02/exam01/IntegerLiteralExample.java
+++ chap02/src/sec02/exam01/IntegerLiteralExample.java
@@ -9,7 +9,7 @@ public class IntegerLiteralExample {
 		int var2 = 014;		// 8진수
 		int var3 = 30;		// 10진수(주로 사용)
 		int var4 = 0x5C01;	// 16진수(RGB 색상값 저장)
-		
+
 		System.out.println(var1);
 		System.out.println(var2);
 		System.out.println(var3);
