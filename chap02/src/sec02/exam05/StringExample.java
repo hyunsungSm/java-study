db7f63f8c826caaf0050c72ba94ca7f2ae23d6e9
hyunseong <oohs942230@naver.com>
2025-07-15 17:43:22 +0900

    chap02 실습
    sec01.exam01: 변수 초기화 실습
    sec01.exam02: 변수 초기화를 시간과 분으로 실습
    sec01.exam03: 변수값 교환 실습
    sec01.exam04: 로컬변수 실습
    
    sec02.exam01: int 리터럴 값 나타내는 실습
    sec02.exam02: byte 표현 및 범위에 의한 오버플로우, 언더플로우 실습
    sec02.exam03: long 리터럴 값 나타내는 실습
    sec02.exam04: char 실습
    sec02.exam05: String 실습
    sec02.exam06: Escape묹자 실습

---
+++ chap02/src/sec02/exam05/StringExample.java
@@ -0,0 +1,14 @@
+package sec02.exam05;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// String: 참조타입, 타입이 대문자로 시작함에 주의!
+		String name = "김재현";
+		String job = "프로그래머";
+		
+		System.out.println(name);
+		System.out.println(job);
+	}
+
+}
