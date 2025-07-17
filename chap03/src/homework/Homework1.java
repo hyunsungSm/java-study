871d0215a7abef29767f7e0f2b096063165e54aa
hyunseong <oohs942230@naver.com>
2025-07-17 17:45:43 +0900

    수정사항

--- chap03/src/homework/Homework1.java
+++ chap03/src/homework/Homework1.java
@@ -7,8 +7,8 @@ public class Homework1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("말해보세요: ");
-		String speak = sc.next();
-		String result = (speak == "온다") ? "간다" : "온다";
+		String speak = sc.nextLine();
+		String result = speak.equals("간다") ? "온다" : "간다";
 		System.out.print("앵무새: " + result);
 	}
 
